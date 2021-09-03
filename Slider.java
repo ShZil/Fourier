680c6a9cd6f89e96c35f9c248377f519503f6f75
ShZil <shzil333@gmail.com>
2021-09-04 00:57:07 +0300

    idk why this is still here, probably should delete it
    the changes are just indentation and fixed Rectangle.contains(Point) to Rectange.contains(int,int) on line Slider.java:20.

--- Slider.java
+++ Slider.java
@@ -2,24 +2,24 @@ import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class Slider {
-  Rectangle box;
-  Color c;
-  float min;
-  float max;
-  float jump;
+    Rectangle box;
+    Color c;
+    float min;
+    float max;
+    float jump;
 
-  Slider(int x, int y, int w, int h, Color clr, float min, float max, float jump) {
-    box = new Rectangle(x, y, w, h);
-    c = clr;
-    this.min = min;
-    this.max = max;
-    this.jump = jump;
-  }
-
-  float click(int x, int y) {
-    if (box.contains(new Point2D(x, y))) {
-      return this.box.width / jump;
+    Slider(int x, int y, int w, int h, Color clr, float min, float max, float jump) {
+        box = new Rectangle(x, y, w, h);
+        c = clr;
+        this.min = min;
+        this.max = max;
+        this.jump = jump;
     }
-    return Float.NaN;
-  }
+
+    float click(int x, int y) {
+        if (box.contains(x, y)) {
+            return this.box.width / jump;
+        }
+        return Float.NaN;
+    }
 }
